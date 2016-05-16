6b5fbe30a9d778b314446842f8e98b0fbbb86600
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-05-16 00:49:09 -0300

    Using mocha docker image

--- escualo_install.sh
+++ escualo_install.sh
@@ -5,4 +5,4 @@ echo "[Escualo::RSpecServer] Fetching GIT revision"
 echo -n $REV > version
 
 echo "[Escualo::RSpecServer] Pulling docker image"
-docker pull mumuki/mumuki-rspec-worker
+docker pull mumuki/mumuki-mocha-worker
