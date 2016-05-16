876ba6127505951f4660829228635403e9359ff7
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-05-16 19:22:27 -0300

    Processing cookie

--- lib/query_hook.rb
+++ lib/query_hook.rb
@@ -7,12 +7,27 @@ class QueryHook < Mumukit::Templates::FileHook
 
 
   def compile_file_content(r)
-    "function mumukiConsolePrettyPrint(e) {
-        if (e instanceof Function) return '<function>';
-        return JSON.stringify(e);
-     }
-#{r.extra}\n#{r.content}\nconsole.log('=> ' + mumukiConsolePrettyPrint(#{r.query}))"
+<<javascript
+function mumukiConsolePrettyPrint(e) {
+    if (e instanceof Function) return '<function>';
+    return JSON.stringify(e);
+ }
+
+#{r.extra}
+
+#{r.content}
+
+#{compile_cookie(r.cookie)}
+
+console.log('=> ' + mumukiConsolePrettyPrint(#{r.query}))
+javascript
   end
+
+  def compile_cookie(cookie)
+    return if cookie.blank?
+    cookie.map { |query| "try { #{query} } catch (e) {}" }
+  end
+
   def command_line(filename)
     "node #{filename}"
   end
