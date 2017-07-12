060e4fdbd22bb5125a89098bda80127e2a8b4e48
Franco Bulgarelli <franco@mumuki.org>
2017-07-11 21:00:23 -0300

    Fixing tests

--- lib/expectations_hook.rb
+++ lib/expectations_hook.rb
@@ -4,4 +4,8 @@ class JavascriptExpectationsHook < Mumukit::Templates::MulangExpectationsHook
   def language
     'JavaScript'
   end
+
+  def default_smell_exceptions
+    %w(UsesCut UsesFail UsesUnificationOperator HasRedundantReduction HasRedundantParameter)
+  end
 end
