305f0369ce5b7df575ceeb420fa80b43e33dd435
Rodrigo Alfonso <rodri042@gmail.com>
2019-02-14 18:54:35 -0300

    Moving regexps to constants

--- lib/query_hook.rb
+++ lib/query_hook.rb
@@ -1,6 +1,10 @@
 class JavascriptQueryHook < Mumukit::Templates::FileHook
   isolated true
 
+  VAR_REGEXP = /^ *(?:var|let) ([a-zA-Z_$][a-zA-Z_$0-9]*)/
+  VAR_ASSIGN_REGEXP = /^ *(?:var|let) ([a-zA-Z_$][a-zA-Z_$0-9]*) *=/
+  CONST_ASSIGN_REGEXP = /^ *const ([a-zA-Z_$][a-zA-Z_$0-9]*) *=/
+  
   def tempfile_extension
     '.js'
   end
@@ -47,17 +51,17 @@ javascript
 
   def compile_sentences(cookie)
     with_no_duplicated_declarations(cookie.map do |query|
-      var_matches = query.match var_regexp
-      var_assign_matches = query.match var_assign_regexp
-      const_assign_matches = query.match const_assign_regexp
+      var_matches = query.match VAR_REGEXP
+      var_assign_matches = query.match VAR_ASSIGN_REGEXP
+      const_assign_matches = query.match CONST_ASSIGN_REGEXP
 
       if const_assign_matches
-        declaration_with_assignment 'const', const_assign_matches[1], query.gsub(const_assign_regexp, '')
+        declaration_with_assignment 'const', const_assign_matches[1], query.gsub(CONST_ASSIGN_REGEXP, '')
       elsif var_matches
         name = var_matches[1]
 
         if var_assign_matches
-          declaration_with_assignment 'var', var_assign_matches[1], query.gsub(var_assign_regexp, '')
+          declaration_with_assignment 'var', var_assign_matches[1], query.gsub(VAR_ASSIGN_REGEXP, '')
         else
           "var #{name}"
         end
@@ -69,9 +73,9 @@ javascript
 
   def with_no_duplicated_declarations(sentences)
     sentences.select.with_index do |line, index|
-      next line if !line.match(var_regexp) && !line.match(var_assign_regexp) && !line.match(const_assign_regexp)
+      next line if !line.match(VAR_REGEXP) && !line.match(VAR_ASSIGN_REGEXP) && !line.match(CONST_ASSIGN_REGEXP)
 
-      name = (line.match(var_assign_regexp) || line.match(var_regexp) || line.match(const_assign_regexp))[1]
+      name = (line.match(VAR_ASSIGN_REGEXP) || line.match(VAR_REGEXP) || line.match(CONST_ASSIGN_REGEXP))[1]
       sentences.slice(0, index).none? { |previousLine| is_declaration? previousLine, name }
     end
   end
@@ -85,26 +89,14 @@ javascript
   end
 
   def is_var_declaration?(line, name)
-    var_matches = line.match var_regexp
-    var_assign_matches = line.match var_assign_regexp
+    var_matches = line.match VAR_REGEXP
+    var_assign_matches = line.match VAR_ASSIGN_REGEXP
 
     (var_matches && var_matches[1] == name) || (var_assign_matches && var_assign_matches[1] == name)
   end
 
   def is_const_declaration?(line, name)
-    const_assign_matches = line.match const_assign_regexp
+    const_assign_matches = line.match CONST_ASSIGN_REGEXP
     const_assign_matches && const_assign_matches[1] == name
-  end
-
-  def var_regexp
-    /^ *(?:var|let) ([a-zA-Z_$][a-zA-Z_$0-9]*)/
-  end
-
-  def var_assign_regexp
-    /^ *(?:var|let) ([a-zA-Z_$][a-zA-Z_$0-9]*) *=/
-  end
-
-  def const_assign_regexp
-    /^ *const ([a-zA-Z_$][a-zA-Z_$0-9]*) *=/
   end
 end
