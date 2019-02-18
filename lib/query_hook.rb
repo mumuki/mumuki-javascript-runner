c0dd5220fc0b7f56ac84e1f78cae04c58a88dd76
Franco Bulgarelli <franco@mumuki.org>
2019-02-18 15:46:04 -0300

    Style fixes

--- lib/query_hook.rb
+++ lib/query_hook.rb
@@ -4,7 +4,7 @@ class JavascriptQueryHook < Mumukit::Templates::FileHook
   VAR_REGEXP = /^ *(?:var|let) +([a-zA-Z_$][a-zA-Z_$0-9]*)/
   VAR_ASSIGN_REGEXP = /^ *(?:var|let) +([a-zA-Z_$][a-zA-Z_$0-9]*) *=/
   CONST_ASSIGN_REGEXP = /^ *const +([a-zA-Z_$][a-zA-Z_$0-9]*) *=/
-  
+
   def tempfile_extension
     '.js'
   end
@@ -50,7 +50,7 @@ javascript
   private
 
   def compile_sentences(cookie)
-    with_no_duplicated_declarations(cookie.map do |query|
+    without_duplicated_declarations(cookie.map do |query|
       var_matches = query.match VAR_REGEXP
       var_assign_matches = query.match VAR_ASSIGN_REGEXP
       const_assign_matches = query.match CONST_ASSIGN_REGEXP
@@ -71,12 +71,12 @@ javascript
     end)
   end
 
-  def with_no_duplicated_declarations(sentences)
+  def without_duplicated_declarations(sentences)
     sentences.select.with_index do |line, index|
       next line if !line.match(VAR_REGEXP) && !line.match(VAR_ASSIGN_REGEXP) && !line.match(CONST_ASSIGN_REGEXP)
 
       name = (line.match(VAR_ASSIGN_REGEXP) || line.match(VAR_REGEXP) || line.match(CONST_ASSIGN_REGEXP))[1]
-      sentences.slice(0, index).none? { |previousLine| is_declaration? previousLine, name }
+      sentences.slice(0, index).none? { |previous_line| declaration? previous_line, name }
     end
   end
 
@@ -84,18 +84,17 @@ javascript
     "#{type} #{name} = (function() { try { return #{expression} } catch(e) { return undefined } })()"
   end
 
-  def is_declaration?(line, name)
-    is_var_declaration?(line, name) || is_const_declaration?(line, name)
+  def declaration?(line, name)
+    var_declaration?(line, name) || const_declaration?(line, name)
   end
 
-  def is_var_declaration?(line, name)
+  def var_declaration?(line, name)
     var_matches = line.match VAR_REGEXP
     var_assign_matches = line.match VAR_ASSIGN_REGEXP
-
     (var_matches && var_matches[1] == name) || (var_assign_matches && var_assign_matches[1] == name)
   end
 
-  def is_const_declaration?(line, name)
+  def const_declaration?(line, name)
     const_assign_matches = line.match CONST_ASSIGN_REGEXP
     const_assign_matches && const_assign_matches[1] == name
   end
